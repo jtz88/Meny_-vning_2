5a44ceb39a7722a23049b367f654fa7b217c4c7f
Test <arne9345789234@banarne.fi>
2020-03-12 16:17:04 +0100

    Add project files.

---
+++ Meny_Övning_2/MainMenu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+namespace Meny_Övning_2
+{
+    public class MainMenu
+    {
+        public int DisplayMenu()
+        {
+            //Metod för att generera huvudmeny och returnera användar val
+
+            System.Console.WriteLine("\nVälkommen till huvudmenyn. Välj vilken funktion du vill testa!\n");
+            System.Console.WriteLine("1.) Ungdom eller pensionär");
+            System.Console.WriteLine("2.) Sällskap");
+            System.Console.WriteLine("3.) Upprepa tio gånger");
+            System.Console.WriteLine("4.) Det tredje ordet");
+            System.Console.WriteLine("5.) Avsluta programmet");
+            System.Console.Write("\nMata in dit val: ");
+
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+
+        }
+
+        public void PriceForParty()
+        {
+            Console.WriteLine("\nHur många är ni i ert sällskap?");
+            var result = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < result; i++)
+            {
+                /* generera consolewriteline x antal ggr baserat på antal i sällskap
+
+                deklarera variabel utanför If - sats som adderar += totalt pris
+
+                Person 1 ålder: */
+
+                Console.WriteLine($"Ålder på person {i}: ");
+
+
+            }
+        }
+
+        public void SplitAString()
+        {
+            // Metod för splitta användarens inmatning och skriva ut tredje index
+
+            Console.WriteLine("\nSkriv en mening med minst 3 ord, det tredje ordet kommer att skrivas ut: ");
+            string input = Console.ReadLine();
+
+            string[] splitResult = input.Split(' ');
+            Console.WriteLine($"Det tredje ordet är: {splitResult[2]}");
+
+
+        }
+
+        public void RepeatTenTimes()
+        {
+            // Metod för att upprepa användarens inamtning 10 ggr
+
+            Console.WriteLine("Skriv en godtycklig text, denna kommer att upprepas tio gånger\n");
+            string text = Console.ReadLine();
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write($"{i}. {text} ");
+            }
+        }
+
+        public void YoungOrOld()
+        {
+            // Metod för att avgöra användarens ålder och skriva ut korrekt biljettpris
+
+            Console.WriteLine("Hur gammal är du?");
+
+            var result = Console.ReadLine();
+            int age = Convert.ToInt32(result);
+
+            if (age < 20 )
+            {
+                Console.WriteLine("\nUngdomspris: 80kr\n");
+            }
+            else if (age > 64)
+            {
+                Console.WriteLine("\nPensionärspris: 90kr\n");
+            }
+            else
+            {
+                Console.WriteLine("\nStandardpris: 120kr\n");
+            }
+
+        }
+    }
+}
---
+++ Meny_Övning_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Meny_Övning_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instansera MainMenu klassen
+
+            MainMenu menu = new MainMenu();
+
+            // Evighetsloop tills userinput är 4
+
+            int userInput = 0;
+            do
+            {
+                switch (userInput)
+                {
+                    case 1:
+                        menu.YoungOrOld();
+                        break;
+                    case 2:
+                        menu.PriceForParty();
+                        break;
+                    case 3:
+                        menu.RepeatTenTimes();
+                        break;
+                    case 4:
+                        menu.SplitAString();
+                        break;
+                    default:
+                        break;
+                }
+
+                userInput = menu.DisplayMenu();
+            } while (userInput != 5);
+
+        }
+    }
+}
